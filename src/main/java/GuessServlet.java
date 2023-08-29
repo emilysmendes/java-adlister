9dfaa984a8ac7c4321072a211523e563f5156c6f
Emily Svetcos <emilysvetcos@gmail.com>
2023-08-29 15:58:37 -0500

    added file to missing merges

--- src/main/java/GuessServlet.java
+++ src/main/java/GuessServlet.java
@@ -11,6 +11,7 @@ public class GuessServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/guess.jsp").forward(req, resp);
+
     }
 
     @Override
