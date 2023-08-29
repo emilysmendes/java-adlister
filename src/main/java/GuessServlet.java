9699f7e8d977d232bbde0bbd6a9bc9e0fd7f9770
Emily Svetcos <emilysvetcos@gmail.com>
2023-08-29 15:58:55 -0500

    updated changes

--- src/main/java/GuessServlet.java
+++ src/main/java/GuessServlet.java
@@ -1,29 +1,29 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Random;
-
-@WebServlet ("/guess")
-public class GuessServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/guess.jsp").forward(req, resp);
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Random random = new Random();
-        int randomNum = random.nextInt(3) + 1;
-        int userGuess = Integer.parseInt(req.getParameter("userGuessNumber"));
-
-        if(randomNum == userGuess) {
-            resp.sendRedirect("/correct");
-        } else {
-            resp.sendRedirect("/incorrect");
-        }
-    }
-}
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.Random;
+//
+//@WebServlet ("/guess")
+//public class GuessServlet extends HttpServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        req.getRequestDispatcher("/guess.jsp").forward(req, resp);
+//
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        Random random = new Random();
+//        int randomNum = random.nextInt(3) + 1;
+//        int userGuess = Integer.parseInt(req.getParameter("userGuessNumber"));
+//
+//        if(randomNum == userGuess) {
+//            resp.sendRedirect("/correct");
+//        } else {
+//            resp.sendRedirect("/incorrect");
+//        }
+//    }
+//}
