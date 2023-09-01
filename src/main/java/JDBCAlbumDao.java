cb1954fa09afc95f1577fb5a8f9b48a1370f02b3
Emily Svetcos <emilysvetcos@gmail.com>
2023-09-01 13:40:32 -0500

    saving changes

--- src/main/java/JDBCAlbumDao.java
+++ src/main/java/JDBCAlbumDao.java
@@ -1,2 +1,92 @@
-package PACKAGE_NAME;public class JDBCAlbumDao {
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class JDBCAlbumDao {
+
+    private Connection connection;
+
+    public JDBCAlbumDao() {
+        try {
+            DriverManager.registerDriver(new Driver() {
+                @Override
+                public Connection connect(String url, Properties info) throws SQLException {
+                    return null;
+                }
+
+                @Override
+                public boolean acceptsURL(String url) throws SQLException {
+                    return false;
+                }
+
+                @Override
+                public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+                    return new DriverPropertyInfo[0];
+                }
+
+                @Override
+                public int getMajorVersion() {
+                    return 0;
+                }
+
+                @Override
+                public int getMinorVersion() {
+                    return 0;
+                }
+
+                @Override
+                public boolean jdbcCompliant() {
+                    return false;
+                }
+
+                @Override
+                public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+                    return null;
+                }
+            });
+            this.connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/codeup_test_db?allowPublicKeyRetrieval=true&useSSL=false",
+                    "root",
+                    "codeup"
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int insertAlbum(Album album) {
+        int lastInsertedId = 0;
+        try {
+            Statement statement = connection.createStatement();
+            String insertQuery = String.format("INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('%s', '%s', %d, %f, '%s')",
+                    album.getArtist(),
+                    album.getName(),
+                    album.getReleaseDate(),
+                    album.getSales(),
+                    album.getGenre()
+            );
+            statement.executeUpdate(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                lastInsertedId = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lastInsertedId;
+    }
+
+    public static void main(String[] args) {
+        JDBCAlbumDao albumDao = new JDBCAlbumDao();
+        Album album = new Album(
+                "Kendrick Lamar",
+                "To Pimp a Butterfly",
+                2015,
+                24.2,
+                "Hip hop");
+        int lastInsertedId = albumDao.insertAlbum(album);
+        System.out.println("Added a new album with an id of..." + lastInsertedId);
+    }
 }
