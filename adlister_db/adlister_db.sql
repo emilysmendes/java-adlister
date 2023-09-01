767e520491785a6e21f6aebeaea9a8ee4e9fe49b
Emily Svetcos <emilysvetcos@gmail.com>
2023-09-01 14:01:15 -0500

    applied privelages to user

--- adlister_db/adlister_db.sql
+++ adlister_db/adlister_db.sql
@@ -1,3 +1,9 @@
 CREATE DATABASE adlister_db;
 
 SHOW databases;
+
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'codeup';
+
+GRANT ALL ON adlister_db.* TO 'user'@'localhost';
+
+SHOW GRANTS for 'user'@'localhost';
